9a0ca755f203c4b49e701374850a8ea25f4b89cc
yuvalsidis <yuvalsidis@gmail.com>
2024-03-10 17:56:31 +0200

    Removing files

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,10 +1,8 @@
-import { UserPreview } from '../cmps/UserPreview.jsx'
+
 
 export function Home() {
     return (
         <section className="home">
-            <h2>Home Sweet Home</h2>
-            <UserPreview />
         </section>
     )
 }
