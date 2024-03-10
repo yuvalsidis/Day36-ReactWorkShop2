38e2d2920a25f591ced74478337fc285cdf81075
yuvalsidis <yuvalsidis@gmail.com>
2024-03-10 23:49:12 +0200

    change photos to string value

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,8 +1,18 @@
 
 export function BookPreview({book}){
-    return <article class="bookPreview">
+
+    function bookNameToImage(title){
+        let stringWitoutSpaces = title.replace(/\s/g, "");
+        console.log(stringWitoutSpaces)
+        let toLowerCase = stringWitoutSpaces.toLowerCase()
+        console.log(toLowerCase)
+        return title
+    }
+
+    return <article className="bookPreview">
             <h1>{book.title}</h1>
             <h5>{book.description}</h5>
             <h5>price: {book.listPrice.amount} {book.currencyCode} </h5>
+            <img src="./assets/img/BooksImages/{bookNameToImage(book.title)}.jpg" alt=""></img>
     </article>
 }
