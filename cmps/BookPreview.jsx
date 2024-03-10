d36436d0e8f54b500f713309558a813c795d34ea
yuvalsidis <yuvalsidis@gmail.com>
2024-03-10 23:57:50 +0200

    Add img to BookPreviesJsx

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,18 +1,22 @@
 
 export function BookPreview({book}){
 
-    function bookNameToImage(title){
+    function convertTitleToImgString(title){
         let stringWitoutSpaces = title.replace(/\s/g, "");
         console.log(stringWitoutSpaces)
-        let toLowerCase = stringWitoutSpaces.toLowerCase()
-        console.log(toLowerCase)
-        return title
+        return stringWitoutSpaces
+    }
+
+    function bookNameToImage(title){
+        const newTitle = convertTitleToImgString(title)
+        console.log(newTitle)
+        return `./assets/img/BooksImages/${newTitle}.jpg`
     }
 
     return <article className="bookPreview">
             <h1>{book.title}</h1>
             <h5>{book.description}</h5>
             <h5>price: {book.listPrice.amount} {book.currencyCode} </h5>
-            <img src="./assets/img/BooksImages/{bookNameToImage(book.title)}.jpg" alt=""></img>
+            <img src={bookNameToImage(book.title)} alt=""></img>
     </article>
 }
