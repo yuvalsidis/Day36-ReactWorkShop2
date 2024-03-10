5b6ed29af9cb748e039cfb9f0d1790c027c2b44a
yuvalsidis <yuvalsidis@gmail.com>
2024-03-10 17:41:13 +0200

    init commit

---
+++ RootCmp.jsx
@@ -0,0 +1,12 @@
+import {Home} from './pages/Home.jsx'
+
+export function App() {
+    return <section className="app">
+        <header className="app-header">
+            <h1>My App</h1>
+        </header>
+        <main className="container">
+            <Home/>
+        </main>
+    </section>
+}
---
+++ pages/Home.jsx
@@ -0,0 +1,10 @@
+import { UserPreview } from '../cmps/UserPreview.jsx'
+
+export function Home() {
+    return (
+        <section className="home">
+            <h2>Home Sweet Home</h2>
+            <UserPreview />
+        </section>
+    )
+}
