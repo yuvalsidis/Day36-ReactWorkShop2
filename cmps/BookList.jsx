59f8b532e54d9f7aa340890e86fd0a300552c9b2
yuvalsidis <yuvalsidis@gmail.com>
2024-03-10 23:09:50 +0200

    Add cleanlist class to BookList component

--- cmps/BookList.jsx
+++ cmps/BookList.jsx
@@ -3,7 +3,7 @@ import { BookPreview } from "./BookPreview.jsx"
 export function BookList({books}){
 
 
-    return <ul className = "book-list">
+    return <ul className = "book-list clean-list ">
         {
             books.map(book => <li key={book.id}>
                  <BookPreview book={book}/>
