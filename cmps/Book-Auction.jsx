97c97f7525d692d6f3219d78542b12b18b015cf3
yuvalsidis <yuvalsidis@gmail.com>
2024-03-11 16:18:41 +0200

    Add another component book auction to book preview

---
+++ cmps/Book-Auction.jsx
@@ -0,0 +1,4 @@
+export function BookAuction(){
+    console.log('Book auction work')
+    return <button className="remove-btn">X</button>
+}
--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,6 +1,7 @@
+import { BookAuction } from "./Book-Auction.jsx";
 
 export function BookPreview({book}){
-
+    
     function convertTitleToImgString(title){
         let stringWitoutSpaces = title.replace(/\s/g, "");
         console.log(stringWitoutSpaces)
@@ -14,9 +15,11 @@ export function BookPreview({book}){
     }
 
     return <article className="bookPreview">
+            
             <h1>{book.title}</h1>
             <h5>{book.description}</h5>
             <h5>price: {book.listPrice.amount} {book.currencyCode} </h5>
             <img src={bookNameToImage(book.title)} alt=""></img>
+            <BookAuction/>
     </article>
 }
