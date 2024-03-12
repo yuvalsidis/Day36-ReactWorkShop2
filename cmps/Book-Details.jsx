35de3b36fed594a5418162cf89c5cdc0c0a7775c
yuvalsidis <yuvalsidis@gmail.com>
2024-03-12 21:19:03 +0200

    Modify book detailes

--- cmps/Book-Details.jsx
+++ cmps/Book-Details.jsx
@@ -1,11 +1,50 @@
+import { utilService } from "../services/util.service.js"
 
 export function BookDetails({ book, onGoBack }) {
+    
+    
+
+    function publishedDateToText() {
+        const currentYear = utilService.getCurrectYear()
+
+        if( book.publishedDate = currentYear  || book.publishedDate === currentYear - 1  ){
+            return 'New'
+        } 
+            
+        console.log('currect year - 10',(currentYear - 10), 'book year', book.publishedDate )
+        if(book.publishedDate < currentYear - 10) {
+            return 'Vintage'
+        }
+    }
+
+    function textLevelSign () {
+        const {pageCount} = book
+        if(pageCount > 500){
+            return 'Serious Reading!'
+        }
+        else if(pageCount > 200 ){
+            return 'Descent Reading!'
+        } 
+        else if(pageCount < 100){
+            return 'Light Reading!'
+        }        
+    }
+
+    const styleTextLevel = {
+                            backgroundColor: 'gold',
+                            display: 'inline-block',
+                            textDecoration: 'none',
+                            marginLeft: '1em'
+                           }
+
     return <section className="book-details">
         <div className="image-details">
             <img src={book.thumbnail} alt=""></img>
         </div>
         <div className="book-details-description">
             <button className="goBack-btn" onClick={onGoBack}>Go Back</button>
+            <h1 style={styleTextLevel}>{textLevelSign()}</h1>
+            <h1 style={{...styleTextLevel, backgroundColor: 'orange'}}>{publishedDateToText()}</h1>
             <div className="DescriptionLineContent">
                 <h1>title:</h1>
                 <p>{book.title}</p>
